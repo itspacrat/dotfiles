0a13c630b42d3abdfd02e22603ecc56644c22dc1
Blake "PacRat" G. <blakemichaelgaynor@gmail.com>
2025-06-30 18:16:38 -0700

    Update fish_prompt.fish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 # Override the fish prompt with the date on one line, user@host on another, and git branch
 function fish_prompt
 
@@ -6,17 +10,17 @@ function fish_prompt
 
     echo # top newline
     # date | user@host 2nd line
-    echo (set_color brmagenta)(date "+%d %b,")(set_color magenta) (date "+%H:%M:%S")(set_color brblue)" | "(set_color brgreen)(whoami)(set_color brblue)@(set_color normal)(hostname)
+    echo (set_color brmagenta)(date "+%d %b,")(set_color magenta) (date "+%Y @ %H:%M:%S")(set_color brblue)" | "(set_color brgreen)(whoami)(set_color brblue)@(set_color normal)(hostname)
 
     # dynamically switch prompt bottom lines 
     # if there is a git branch detected 
     if test $branchlength -gt 0
         # if there is a branch:
-        echo (set_color --underline magenta)(prompt_pwd)(set_color normal)
-        echo (set_color brmagenta)"["(set_color yellow)(fish_git_prompt %s)(set_color brmagenta)"]"(set_color --bold brgreen)" ➜  "
+        echo (set_color --underline green)(prompt_pwd)(set_color normal)
+        echo (set_color brmagenta)"["(set_color yellow)(fish_git_prompt %s)(set_color brmagenta)"]"(set_color --bold brmagenta)"➜  "(set_color normal)
     else
         # if there is not a branch:
         echo (set_color --underline green)(prompt_pwd)(set_color normal)
         echo (set_color --bold blue)"➜  "(set_color normal)
     end
-end
+end 
